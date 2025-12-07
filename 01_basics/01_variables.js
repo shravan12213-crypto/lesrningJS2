dd95ed068085c55ffa50ab9237d76ab3afc4bfd1
shravan12213-crypto <shravan12213@gmail.com>
2025-12-07 19:27:06 +0000

    02files

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountid=12345
+let accoutemail= "name@gmail.com"
+var accountpassword="1234456"
+accountcity="pune"
+let accountstate
+console.log(accountid);
+// const accountid=3 not possible to re enter the value of const
+
+console.table([accountid,accountpassword,accoutemail,accountcity,accountstate])
+
+/*
+prefer not to use var
+*/
